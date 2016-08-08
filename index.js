0dd7467384380933ef9c9b129163aaa10daa51e8
josephquested <josephquested@gmail.com>
2016-08-09 11:03:08 +1200

    Updates

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
-
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
-
+  if (err) { console.error(err) }
+  console.log(data.body)
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Space'})
