f74f5f9f4a14d41cd01e4fd1342ede323dfe7797
josephquested <josephquested@gmail.com>
2016-08-09 11:10:09 +1200

    Updates

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) { console.error(err) }
-  console.log(data.body)
-
   data = JSON.parse(data.body);
+  console.log(data.body)
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: data.body.name, id: data.body.id})
 })
